3bc25456faf86fd15b5a4d24c80f0434e02d66fe
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-24 19:29:23 -0500

    feat: add mailgen for email body template

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer'
+import Mailgen from 'mailgen'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -35,11 +36,36 @@ export const getbill = (req, res) => {
         }
     }
     let transporter = nodemailer.createTransport(config)
+    let Mailgenerator = new Mailgen({
+        theme: 'default',
+        product: {
+            name: 'Super Coder Ecommerce',
+            link: 'http://coderecomerce.com'
+        }
+    })
+
+    let content = {
+        body: {
+            intro: 'Your bill has arrived!',
+            table: {
+                data: [
+                    {
+                        item: 'Super bicicleta de ruedas cuadradas',
+                        description: 'Diviertete usando esta super bicicleta',
+                        price: 'ARS$ 1000.00'
+                    }
+                ]
+            },
+            outro: 'Sincerily yours, El profe'
+        }
+    }
+    let mail = Mailgenerator.generate(content)
+
     let message = {
         from: process.env.NODEMAILER_USER,
         to: req.body.useremail,
         subject: 'Gracias por tu compra',
-        html: '<h1>gracias por tu compra</h1><hr /><p style="color: blue">Pronto tendras tus productos en tu casa</p>'
+        html: mail
     }
     transporter.sendMail(message)
         .then(() => res.status(201).json({ status: 'success'}))
